5a4baae726a33372867ed73743ff75de5cd4e44e
tankw <tantk113@gmail.com>
2023-09-22 14:58:28 +0700

    savecartinlocal

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,13 @@
-export let cart=[{
+export let cart= JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+cart = [{
   productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
   quantity: 2
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 2
+}];
 }
-
-];
 
 export function addToCart( productId){
   
@@ -24,6 +25,7 @@ export function addToCart( productId){
       quantity :1  
     })
   }
+  saveToStorge();
 }
 
 export function removeFromCart(id){
@@ -34,4 +36,10 @@ export function removeFromCart(id){
     newCart.push(cartitem);
   })
   cart = newCart;
+  saveToStorge();
+}
+
+function saveToStorge(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+
 }
